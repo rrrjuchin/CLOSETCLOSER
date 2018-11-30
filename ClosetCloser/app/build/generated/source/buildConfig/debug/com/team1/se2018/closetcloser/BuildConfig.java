941732ec3e7e076c56dc063a04d5841d1c9218aa
rrrjuchin <rrrjuchin@gmail.com>
2018-12-01 00:55:50 +0900

    jk

---
+++ ClosetCloser/app/build/generated/source/buildConfig/debug/com/team1/se2018/closetcloser/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.team1.se2018.closetcloser;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.team1.se2018.closetcloser";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
