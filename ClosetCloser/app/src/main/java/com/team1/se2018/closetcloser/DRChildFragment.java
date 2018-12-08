bb7559d8ddf4b9fc6feb857c0dbc43b8e1992aee
input0 <jason9605@naver.com>
2018-12-09 08:30:36 +0900

    Add files via upload

--- ClosetCloser/app/src/main/java/com/team1/se2018/closetcloser/DRChildFragment.java
+++ ClosetCloser/app/src/main/java/com/team1/se2018/closetcloser/DRChildFragment.java
@@ -3,6 +3,7 @@ package com.team1.se2018.closetcloser;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,21 +13,39 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class DRChildFragment extends Fragment {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class DRChildFragment extends Fragment{
 
     private OnFragmentInteractionListener mListener;
 
-
+    private List<Upload> mUploads;
+    private DatabaseReference mDatabaseRef;
+    private ValueEventListener mDBListener;
+    private TestImageAdapter mAdapter;
+    private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    private FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
 
         View rootView = inflater.inflate(R.layout.fragment_drchild, container, false);
 
         ImageView ivOuter = (ImageView)rootView.findViewById(R.id.outer_image);
         ImageView ivTop = (ImageView)rootView.findViewById(R.id.top_image);
         ImageView ivBottom = (ImageView)rootView.findViewById(R.id.bottom_image);
-
+        downloadimg("user/aouCL05PfURmLaKQG7nO7LQnVRG2/summer/03f955d4-54fe-43cc-ac7a-41c705ace308",ivOuter);
 
 
 
@@ -43,7 +62,24 @@ public class DRChildFragment extends Fragment {
                     + " must implement OnFragmentInteractionListener");
         }
     }
+    public void downloadimg(String img,final ImageView iv){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        storageRef.child(img).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.with(DRChildFragment.this.getActivity())
+                        .load(uri)
+                        .fit()
+                        .into(iv);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
 
+            }
+        });
+    }
     @Override
     public void onDetach() {
         super.onDetach();
